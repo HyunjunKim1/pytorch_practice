a78055040c4121e3bd164ecfdd2e0b1ab97a95aa
hjkim <guswns589898@gmail.com>
2023-12-12 16:34:11 +0900

    Linear Regression

--- pytorch_practice/LinearRegression.py
+++ pytorch_practice/LinearRegression.py
@@ -1,3 +1,36 @@
+#import torch
+#import torch.nn as nn
+#import torch.nn.functional as F
+#import torch.optim as optim
+#
+#torch.manual_seed(1)
+#
+#x_train = torch.FloatTensor([[1], [2], [3]])
+#y_train = torch.FloatTensor([[2], [4], [6]])
+#
+#W = torch.zeros(1, requires_grad=True)
+#b = torch.zeros(1, requires_grad=True)
+#
+#optimizer = optim.SGD([W, b], lr=0.01)
+#
+#nb_epochs = 1999 
+#for epoch in range(nb_epochs + 1):
+#
+#    hypothesis = x_train * W + b
+#
+#    cost = torch.mean((hypothesis - y_train) ** 2)
+#
+#    optimizer.zero_grad()
+#    cost.backward()
+#    optimizer.step()
+#
+#    if epoch % 100 == 0:
+#        print('Epoch {:4d}/{} W: {:.3f}, b: {:.3f} Cost: {:.6f}'.format(
+#            epoch, nb_epochs, W.item(), b.item(), cost.item()
+#        ))
+
+# nn.Module로 구현
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -5,26 +38,36 @@ import torch.optim as optim
 
 torch.manual_seed(1)
 
-x_train = torch.FloatTensor([[1], [2], [3]])
-y_train = torch.FloatTensor([[2], [4], [6]])
+x_train = torch.FloatTensor([[1],[2],[3]])
+y_train = torch.FloatTensor([[2],[4],[6]])
 
-W = torch.zeros(1, requires_grad=True)
-b = torch.zeros(1, requires_grad=True)
+# input_dim = 1, output_dim = 1
+model = nn.Linear(1,1)
 
-optimizer = optim.SGD([W, b], lr=0.01)
+#print(list(model.parameters()))
 
-nb_epochs = 1999 
-for epoch in range(nb_epochs + 1):
+# 경사하강법
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01) 
 
-    hypothesis = x_train * W + b
+nb_epochs = 2000
+for epoch in range(nb_epochs+1):
 
-    cost = torch.mean((hypothesis - y_train) ** 2)
-
+    prediction = model(x_train)
+    cost = F.mse_loss(prediction, y_train)
     optimizer.zero_grad()
+
+    # 비용 함수를 미분하여 gradient 계산
     cost.backward()
+    # W와 b를 업데이트
     optimizer.step()
 
     if epoch % 100 == 0:
-        print('Epoch {:4d}/{} W: {:.3f}, b: {:.3f} Cost: {:.6f}'.format(
-            epoch, nb_epochs, W.item(), b.item(), cost.item()
-        ))
+    # 100번마다 로그 출력
+      print('Epoch {:4d}/{} Cost: {:.6f}'.format(
+          epoch, nb_epochs, cost.item()
+      ))
+
+new_var = torch.FloatTensor([[4.0]])
+pred_y = model(new_var)
+
+print(pred_y)
--- pytorch_practice/MultiLinearRegression.py
+++ pytorch_practice/MultiLinearRegression.py
@@ -5,3 +5,65 @@ import torch.optim as optim
 
 torch.manual_seed(1)
 
+## 점수 훈련 데이터
+#x_train  =  torch.FloatTensor([[73,  80,  75], 
+#                               [93,  88,  93], 
+#                               [89,  91,  80], 
+#                               [96,  98,  100],   
+#                               [73,  66,  70]])  
+#y_train  =  torch.FloatTensor([[152],  [185],  [180],  [196],  [142]])
+#
+#W = torch.zeros((3, 1), requires_grad=True)
+#b = torch.zeros(1, requires_grad=True)
+#
+#optimizer = optim.SGD([W, b], lr=1e-5)
+#
+#
+#nb_epochs = 200
+#for epoch in range(nb_epochs + 1):
+#
+#    # H(x) 계산
+#    # 편향 b는 브로드 캐스팅되어 각 샘플에 더해집니다.
+#    hypothesis = x_train.matmul(W) + b
+#
+#    # cost 계산
+#    cost = torch.mean((hypothesis - y_train) ** 2)
+#
+#    # cost로 H(x) 개선
+#    optimizer.zero_grad()
+#    cost.backward()
+#    optimizer.step()
+#
+#    print('Epoch {:4d}/{} hypothesis: {} Cost: {:.6f}'.format(
+#        epoch, nb_epochs, hypothesis.squeeze().detach(), cost.item()
+#    ))
+
+# 다중선형회귀 데이터
+x_train = torch.FloatTensor([[73, 80, 75],
+                             [93, 88, 93],
+                             [89, 91, 90],
+                             [96, 98, 100],
+                             [73, 66, 70]])
+y_train = torch.FloatTensor([[152], [185], [180], [196], [142]])
+
+model = nn.Linear(3,1)
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-5)
+
+nb_epochs = 2000
+for epoch in range(nb_epochs+1):
+
+    prediction = model(x_train)
+    cost = F.mse_loss(prediction, y_train) # <== 파이토치에서 제공하는 평균 제곱 오차 함수
+
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+    if epoch % 100 == 0:
+      print('Epoch {:4d}/{} Cost: {:.6f}'.format(
+          epoch, nb_epochs, cost.item()
+      ))
+
+new_var =  torch.FloatTensor([[73, 80, 75]]) 
+pred_y = model(new_var) 
+print(pred_y)
